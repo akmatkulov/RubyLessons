a066e86e920170eaa4dc68048a80510350e4cc01
Sovetbek Akmatkulov <akmatkulovs991@gmail.com>
2022-02-26 11:32:45 +0300

    Update vehicle

--- chapter3/vehicle.rb
+++ chapter3/vehicle.rb
@@ -18,5 +18,19 @@ class Vehicle
   def mileage
     @odometr / @gas_used
   end
-  
+
+end
+
+class Car < Vehicle
+end
+
+class Truck < Vehicle
+  attr_accessor :cargo
+  def load_bed(contents)
+    puts "Securing #{contents} in the truck bed."
+    @cargo = contents
+  end 
+end
+
+class Motorcycle < Vehicle
 end
