1c290c79584faab0dd6684bd5d62c6519a037508
Sovetbek Akmatkulov <akmatkulovs991@gmail.com>
2022-03-10 22:31:01 +0300

    Final update code

--- chapter6/hash.rb
+++ chapter6/hash.rb
@@ -4,9 +4,22 @@ File.open("reviews.txt") do |file|
     lines = file.readlines
 end 
 
-relevant_line = lines.find_all { |line| line.include?("Truncated")}
+relevant_line = lines.find_all { |line| line.include?("Truncated") }
+reviews = relevant_line.reject { |line| line.include?("--") }
 
-puts relevant_line
+def find_adjective(string)
+    words = string.split(" ")
+    index = words.find_index("is")
+    words[index + 1]
+end 
+
+adjectives = reviews.map do |line|
+    adjective = find_adjective(line)
+        "'#{adjective.capitalize}'"
+end 
+
+
+puts adjectives
 
 
 
